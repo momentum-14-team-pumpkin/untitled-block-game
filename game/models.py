76bd90bf63f29b1c62d4aa69e2e2dc4b334cd98b
StephenTurk90 <stephenjturk@gmail.com>
2022-10-09 14:57:31 -0400

    update models to include ScoreList, Score, and Time

--- game/models.py
+++ game/models.py
@@ -6,3 +6,24 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class ScoreList(models.Model):
+    player = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name = 'scores')
+    time = models.ForeignKey('Time',on_delete=models.CASCADE, related_name = 'time')
+    score = models.ForeignKey('Score', on_delete=models.CASCADE, related_name = 'scores')
+
+
+    def __str__(self):
+        return f'{self.owner} : {self.time} : {self: score} points'
+
+
+class Time(models.Model):
+
+
+class Score(models.Model):
+    score = models.IntegerField()
+
+
+class Time(models.Model):
+    time = models.PositiveIntegerField()
