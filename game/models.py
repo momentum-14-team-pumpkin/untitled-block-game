61553ce85b1a2259f852eb2d21962073e8dc8172
StephenTurk90 <stephenjturk@gmail.com>
2022-10-09 17:03:55 -0400

    remove scores; updated leaderboard;  added level, character models; updated Time model

--- game/models.py
+++ game/models.py
@@ -8,22 +8,35 @@ class CustomUser(AbstractUser):
         return self.username
 
 
-class ScoreList(models.Model):
-    player = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name = 'scores')
+class LeaderBoard(models.Model):
+    player = models.ForeignKey('Character',on_delete=models.CASCADE, related_name = 'player')
     time = models.ForeignKey('Time',on_delete=models.CASCADE, related_name = 'time')
-    score = models.ForeignKey('Score', on_delete=models.CASCADE, related_name = 'scores')
-
+    level = models.ForeignKey('Level', on_delete=models.CASCADE, related_name= 'levels')
 
     def __str__(self):
-        return f'{self.owner} : {self.time} : {self: score} points'
+        return f'{self.player} : {self.time} seconds on level {self.level}'
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=15, blank=True, null=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='players')
+
+    def __str__(self):
+        return f'{self.user.username} selected {self.name}'
+
+
+class Level(models.Model):
+    name = models.PositiveIntegerField(null=True, blank=True, default=None)
+
+    def __str__(self):
+        return f'Level: {self.name}'
 
 
 class Time(models.Model):
+    time = models.FloatField(null=True, blank=True, default=None)
+    player = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True, related_name='times')
+    level = models.ForeignKey(Level, on_delete=models.CASCADE, null=True, blank=True, related_name='times')
 
+    def __str__(self):
+        return f'{self.player.username} completed level {self.level.name} in {self.time} seconds'
 
-class Score(models.Model):
-    score = models.IntegerField()
-
-
-class Time(models.Model):
-    time = models.PositiveIntegerField()
