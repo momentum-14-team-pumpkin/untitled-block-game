b79a81191e9659c47f27aacd17dc6cbe5c458bc4
coreygross <grosscor3@gmail.com>
2022-10-10 16:27:55 -0400

    feat: create tile map and background
    Co-authored-by: Alexis <leximainw@gmail.com>
    Co-authored-by: Tom <tomchrismorris@yahoo.com>
    Co-authored-by: Stephen <stephenjturk@gmail.com>

--- static/js/game.js
+++ static/js/game.js
@@ -1,7 +1,8 @@
 let config = {
     type: Phaser.AUTO,
-    width: 1920,
-    height: 1080,
+    width: 1200,
+    height: 800,
+    pixelArt: true,
     physics: {
         default: 'arcade',
         arcade: {
@@ -9,10 +10,29 @@ let config = {
         },
     },
     scene: {
-        preload: preload
+        preload: preload,
+        create: create,
     },
 }
 
+let game = new Phaser.Game(config)
+
 function preload() {
-    this.load.image('bg', 'static/assets/background.png')
+    this.load.image('bg', '/static/assets/background.png')
+    this.load.image('tiles', '/static/assets/drawtiles-spaced.png')
+    this.load.tilemapCSV('map', '/static/assets/grid.csv')
 }
+
+let platforms
+
+function create() {
+    this.add.image(600, 400, 'bg').setScale(1200/1920)
+    var map = this.make.tilemap({ key: 'map', tileWidth: 40, tileHeight: 40 })
+    var tileset = map.addTilesetImage('tiles', null, 32, 32, 1, 2)
+    var layer = map.createLayer(0, tileset, 40, 40*10)
+    // this.cameras.main.setScroll(-960,-540)
+    // this.cameras.main.setZoom(32)
+
+
+}
+
