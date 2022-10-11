ea91ac3dbddcebca62dcb1dd9939a5a71bd82a7f
StephenTurk90 <stephenjturk@gmail.com>
2022-10-11 15:31:49 -0400

    fix: add user and time urls and views

--- game/urls.py
+++ game/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('game/', views.show_game, name='BLOCK_IN_TIME'),
+    path('users/', views.user_list, name='Users'),
+    path('times/', views.time_trial_list, name='Times')
 ]
--- game/views.py
+++ game/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def show_game(req):
     return render(req, "game/game.html")
 
+
+def user_list(request, pk):
+    user = get_object_or_404(user, pk=pk)
+    return render(request, 'game/user_list.html', {'user': user})
+
+def time_trial_list(request, pk):
+    timetrial = get_object_or_404(time, pk=pk)
+    return rednder(request, 'game/time_trial.html', {'TimeTrial': TimeTrial})
