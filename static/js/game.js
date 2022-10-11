95d02ff546a5af5305ff0293f33fcb7675b7b50c
coreygross <grosscor3@gmail.com>
2022-10-11 10:33:15 -0400

    feat: implement removing and placing blocks left and right
    Co-authored-by: Alexis <leximainw@gmail.com>
    Co-authored-by: Stephen <stephenjturk@gmail.com>
    Co-authored-by: Tom <tomchrismorris@yahoo.com>

--- static/js/game.js
+++ static/js/game.js
@@ -19,11 +19,14 @@ let config = {
 let player
 let gameOver = false
 let cursors
+let map
+let hasBlock = false
+let facing = 'left'
 
 let game = new Phaser.Game(config)
 
 function preload() {
-    this.load.image('bg', '/static/assets/background.png')
+    this.load.image('bg', '/static/assets/city_PNG48.png')
     this.load.image('tiles', '/static/assets/drawtiles-spaced.png')
     this.load.tilemapCSV('map', '/static/assets/grid.csv')
     this.load.image('door', '/static/assets/door.png')
@@ -31,16 +34,17 @@ function preload() {
 }
 
 function create() {
-    this.add.image(600, 400, 'bg').setScale(1200/1920)
+    this.add.image(600, 500, 'bg').setScale(3)
     this.add.image(convertTilesToXPixels(2), convertTilesToYPixels(6), 'door').setScale(40/256)
-    var map = this.make.tilemap({ key: 'map', tileWidth: 40, tileHeight: 40 })
-    var tileset = map.addTilesetImage('tiles', null, 32, 32, 1, 2)
-    var layer = map.createLayer(0, tileset, 40, 40*10)
+    map = this.make.tilemap({ key: 'map', tileWidth: 40, tileHeight: 40 })
+    let tileset = map.addTilesetImage('tiles', null, 32, 32, 1, 2)
+    let layer = map.createLayer(0, tileset, 40, 40*10)
     player = this.physics.add.sprite(convertTilesToXPixels(14), convertTilesToYPixels(5)-4, 'dude')
     player.setCollideWorldBounds(true)
     this.physics.add.existing(player)
     this.physics.add.collider(player, layer)
     map.setCollisionByExclusion([0])
+
 
     this.anims.create({
         key: 'left',
@@ -75,12 +79,14 @@ function update ()
 
     if (cursors.left.isDown)
     {
+        facing = 'left'
         player.setVelocityX(-150)
 
         player.anims.play('left', true)
     }
     else if (cursors.right.isDown)
     {
+        facing = 'right'
         player.setVelocityX(150)
 
         player.anims.play('right', true) 
@@ -89,12 +95,42 @@ function update ()
 
     if (Phaser.Input.Keyboard.JustDown(cursors.space) && player.body.blocked.down)
     {
-        player.y-=50
+        player.y -= 50
     }
-    
+
+    if (Phaser.Input.Keyboard.JustDown(cursors.down))
+    {
+        if (!hasBlock){
+            let point
+            if (facing == 'left'){
+                point = map.worldToTileXY(player.x -42, player.y, true)
+            }
+            else if (facing == 'right'){
+                point = map.worldToTileXY(player.x +42, player.y, true)
+            }
+            if (map.getTileAt(point.x, point.y).index == 2){
+                map.putTileAt(0, point.x, point.y)
+                player.setTint(0x00ff00)
+                hasBlock = true
+            }
+        }
+        else {
+            let point
+            if (facing == 'left'){
+                point = map.worldToTileXY(player.x -42, player.y, true)
+            }
+            else if (facing == 'right'){
+                point = map.worldToTileXY(player.x +42, player.y, true)
+            }
+            if (map.getTileAt(point.x, point.y).index == 0){
+                map.putTileAt(2, point.x, point.y)
+                player.setTint(0xff0000)
+                hasBlock = false
+            }
+        }
+
+    }
 }
-
-
 
 function convertTilesToXPixels(tiles){
     return tiles*40+20
