884ea902014a4704e66a203b414e83ecfbd33c68
Thomas Morris <tomchrismorris@yahoo.com>
2022-10-12 10:52:20 -0400

    feat:adds djoser/token authentication, adds corsheaders, updates views for create/update/destroy ability

--- config/settings.py
+++ config/settings.py
@@ -48,12 +48,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
+    'rest_framework.authtoken',
+    'djoser',
     'game',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -147,5 +151,13 @@ AUTH_USER_MODEL = 'game.CustomUser'
 SIMPLE_BACKEND_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/'
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 django_on_heroku.settings(locals())
 del DATABASES['default']['OPTIONS']['sslmode']
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
