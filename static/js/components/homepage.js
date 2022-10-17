5be6f2c28b6b0e2dd82b1f81a348614cce6186a8
StephenTurk90 <stephenjturk@gmail.com>
2022-10-17 09:09:26 -0400

    added brick-black.png to logo

--- static/js/components/homepage.js
+++ static/js/components/homepage.js
@@ -6,7 +6,7 @@ export const HomePage =()=> {
 
 return(
     <>
-    <h1 style={{fontSize:'70px', fontWeight:'600', backgroundImage:'URL(/static/assets/onetile.png)', backgroundSize:'contain', backgroundRepeat:'repeat', color:'transparent', WebkitBackgroundClip:'text', backgroundClip:'text', textAlign:'center'}}
+    <h1 style={{fontSize:'70px', fontWeight:'600', backgroundImage:'URL(/static/assets/brick-black.png)', backgroundSize:'contain', backgroundRepeat:'repeat', color:'transparent', WebkitBackgroundClip:'text', backgroundClip:'text', textAlign:'center'}}
     >Blocks of Time </h1>
     <header style={{alignItems:'center'}}>
     <NavBar />
