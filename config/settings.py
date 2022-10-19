8b07364fc88d473f1238d9641e9fff86ee45c2aa
StephenTurk90 <stephenjturk@gmail.com>
2022-10-19 10:53:04 -0400

    add: csp to settings

--- config/settings.py
+++ config/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'csp.middleware.CSPMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
