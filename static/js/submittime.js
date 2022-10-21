4134bb42742037b1eda01bcb77bdc8d27d041959
StephenTurk90 <stephenjturk@gmail.com>
2022-10-21 15:00:29 -0400

    fix: leaderboard to use results for pagination
    Co-authored-by: Alexis Mainwaring <leximainw@gmail.com>

--- static/js/submittime.js
+++ static/js/submittime.js
@@ -1,17 +1,12 @@
+console.log("connected")
 function submitTime (time, isFullGame) {
     axios
-    .get(`https://young-plateau-94674.herokuapp.com/user/level/${level}/times/`, {
+    .post(`https://young-plateau-94674.herokuapp.com/levels/${level}/times/`, {
+        time
+    }, {
         headers: {
             Authorization: `Token ${token}`
         },
     })
-    .then((res) =>
-    setUserBestTimesTotal(res.data.sort((a,b,c,d)=> {
-        let timeTotal = a.time + b.time + c.time + d.time
-        if (timeTotal != 0){
-            return timeTotal
-        }
-        return 0   //TODO: use created time ??
-    } 
-    )))
+
 }
