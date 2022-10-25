81df33247293b7cab3441527303f2fad59d9379f
leximainw <leximainw@gmail.com>
2022-10-25 12:19:10 -0400

    feat: make leaderboard left-justified and monospace

--- static/js/components/leaderboard.js
+++ static/js/components/leaderboard.js
@@ -10,7 +10,7 @@ export const Leaderboard =({level}) => {
 
     useEffect(() => {
         axios
-        .get(`https://young-plateau-94674.herokuapp.com/levels/${level}/times/`)
+        .get(`https://blocks-of-time.herokuapp.com/levels/${level}/times/`)
         .then((res) =>
         setBestTimes(res.data.results.sort((a,b)=> {
             let timeDiff = a.time - b.time
@@ -24,6 +24,11 @@ export const Leaderboard =({level}) => {
     
 
     if (bestTimes){
+        let timeEntries = bestTimes.map(timeEntry =>
+            `${convertSecondsToTimestring(timeEntry.time)}...${timeEntry.player}`)
+        const longestEntry = timeEntries.reduce((acc, elem) => Math.max(acc, elem.length), 0)
+        timeEntries = timeEntries.map(text => text.replace('...', Array(longestEntry - text.length + 4).join('.')))
+
         return(
             <>
             <div className="box" style={{color:'white', width:'75%', margin:'auto', fontFamily:'bungee' ,textAlign:'center', paddingBottom:'50px'}}>
@@ -33,20 +38,20 @@ export const Leaderboard =({level}) => {
                     </p>
                     <p style={{fontSize:'1rem', fontStyle:'italic'}}>Must <Link to="/login/" style={{color:'white'}}>Login </Link>To Make Leaderboard</p>
                 </div>
-                <div>
-                    {bestTimes.map((listOfTimes) => (
-                    <div key={listOfTimes.id} 
-                        style={{
-                            textAlign:'center',
-                            margin:'auto',
-                        }}>
-                        <div style={{display:'flex', justifyContent:'center', fontSize:'1.5rem', margin:'auto', textAlign:'center', maxWidth:'80%'}}>
-                            <p>{convertSecondsToTimestring(listOfTimes.time)}</p>
-                            <p>...............{listOfTimes.player}</p>
-                            
-                        </div>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontFamily: 'Inconsolata, Courier, Consolas, "Fira Code", monospace',
+                    fontSize: '2rem',
+                }}>
+                    <div>
+                        {timeEntries
+                            .map((entry, i) => (
+                            <div key={i}>
+                                {entry}
+                            </div>
+                        ))}
                     </div>
-                    ))}
                 </div>
             </div>
             </>
