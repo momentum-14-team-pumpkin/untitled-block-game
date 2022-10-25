9af23f2ad08316d730354133ee598d6e1731486d
coreygross <grosscor3@gmail.com>
2022-10-25 17:26:31 -0400

    feat: game description on homepage

--- static/js/components/gamedetails.js
+++ static/js/components/gamedetails.js
@@ -3,10 +3,10 @@ import "/static/css/index.css"
 
 export const GameDescription = () => {
     return(
-        <p style={{color:'white', fontSize:'1.5rem', width:'min(40rem, 100%)', margin:'auto', marginTop:'5rem'}}>
-            Prof. Pretzel <img src='static/assets/images/Stanley-left.png'></img> fell through a portal & went back in time.<br></br>
-            Move blocks to get to the portals <img src='static/assets/images/portal.gif'></img><br></br>
-            to help Prof. Pretzel get back to his time!
+        <p className='box' style={{color:'white', fontSize:'2rem', width:'min(35rem, 100%)', margin:'auto', marginTop:'1.5rem'}}>
+            Prof. Pretzel <img src='/static/assets/images/Stanley-left.png'></img> fell through a portal and went back in time.<br></br><br></br>
+            Help the Professor move blocks to get to the portals and back to the future. <br></br><br></br>
+            Each portal <img src='/static/assets/images/portal.gif'></img> and level completed gets the Professor one step closer to home. <br></br>
         </p>
     )
 }
--- static/js/components/homepage.js
+++ static/js/components/homepage.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { GameDescription } from "./gamedetails"
 import "/static/css/index.css"
+import { Link } from "react-router-dom"
 
 
 
 export const HomePage =({})=> {
 return(
     <GameDescription />
-)
+    )
 
 }
