e3ad913bcc48b8ea87825f433cc97b89d486b12d
leximainw <leximainw@gmail.com>
2022-10-25 17:43:47 -0400

    refactor: make all leaderboards visually consistent with global level leaderboard

--- static/js/components/fullgameleaderboard.js
+++ static/js/components/fullgameleaderboard.js
@@ -24,6 +24,11 @@ export const FullGameLeaderboard =({}) => {
     
 
     if (bestFullTimes){
+        let timeEntries = bestFullTimes.map((timeEntry, index) =>
+            `${(index + 1 + '.').padEnd(4)}${convertSecondsToTimestring(timeEntry.time)}...${timeEntry.player}`)
+        const longestEntry = timeEntries.reduce((acc, elem) => Math.max(acc, elem.length), 0)
+        timeEntries = timeEntries.map(text => text.replace('...', Array(longestEntry - text.length + 4).join('.')))
+
         return(
             <>
             <div className="box" style={{color:'white', width:'75%', margin:'auto', fontFamily:'bungee' ,textAlign:'center', paddingBottom:'50px'}}>
@@ -33,20 +38,22 @@ export const FullGameLeaderboard =({}) => {
                     </p>
                     <p style={{fontSize:'1rem', fontStyle:'italic'}}>Must <Link to="/login/" style={{color:'white'}}>Login </Link>To Make Leaderboard</p>
                 </div>
-                <div>
-                    {bestFullTimes.map((listOfTimes) => (
-                    <div key={listOfTimes.id} 
-                        style={{
-                            textAlign:'center',
-                            margin:'auto',
-                        }}>
-                        <div style={{display:'flex', justifyContent:'center', fontSize:'1.5rem', margin:'auto', textAlign:'center', maxWidth:'80%'}}>
-                            <p>{convertSecondsToTimestring(listOfTimes.time)}</p>
-                            <p>...............{listOfTimes.player}</p>
-                            
-                        </div>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    fontFamily: 'Inconsolata, Courier, Consolas, "Fira Code", monospace',
+                    fontSize: '2rem',
+                }}>
+                    <div>
+                        {timeEntries
+                            .map((entry, i) => (
+                            <div key={i} style={{
+                                whiteSpace: 'pre',
+                            }}>
+                                {entry}
+                            </div>
+                        ))}
                     </div>
-                    ))}
                 </div>
             </div>
             </>
