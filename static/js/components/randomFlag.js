c9450c97971e915027445daea93c80c9e676ec0a
leximainw <leximainw@gmail.com>
2022-10-25 21:57:30 -0400

    feat: add additional flags

--- static/js/components/randomFlag.js
+++ static/js/components/randomFlag.js
@@ -1,9 +1,79 @@
 import React from "react"
 import { AceFlag } from "./ace"
+import "/static/css/stripes.css"
 
 export const RandomFlag = () => {
     const flags = [
-        <AceFlag height='100%' />
+        <div className="stripes" direction="horizontal" style={{
+            width: '100%',
+            height: '100%',
+        }}>
+            <div style={{backgroundColor: "#e50000"}}></div>
+            <div style={{backgroundColor: "#ff8d00"}}></div>
+            <div style={{backgroundColor: "#ffee00"}}></div>
+            <div style={{backgroundColor: "#028121"}}></div>
+            <div style={{backgroundColor: "#004cff"}}></div>
+            <div style={{backgroundColor: "#770088"}}></div>
+        </div>,
+        <div className="stripes" direction="horizontal" style={{
+            width: '100%',
+            height: '100%',
+        }}>
+            <div style={{backgroundColor: "#d62800"}}></div>
+            <div style={{backgroundColor: "#f07527"}}></div>
+            <div style={{backgroundColor: "#ff9b56"}}></div>
+            <div style={{backgroundColor: "#ffffff"}}></div>
+            <div style={{backgroundColor: "#d462a6"}}></div>
+            <div style={{backgroundColor: "#b55592"}}></div>
+            <div style={{backgroundColor: "#a40062"}}></div>
+        </div>,
+        <div className="stripes" direction="horizontal" style={{
+            width: '100%',
+            height: '100%',
+        }}>
+            <div style={{backgroundColor: "#018e71"}}></div>
+            <div style={{backgroundColor: "#21cfab"}}></div>
+            <div style={{backgroundColor: "#99e9c2"}}></div>
+            <div style={{backgroundColor: "#ffffff"}}></div>
+            <div style={{backgroundColor: "#7cafe3"}}></div>
+            <div style={{backgroundColor: "#4f47cd"}}></div>
+            <div style={{backgroundColor: "#3b1379"}}></div>
+        </div>,
+        <div className="stripes" direction="horizontal" style={{
+            width: '100%',
+            height: '100%',
+        }}>
+            <div style={{backgroundColor: "#d60270"}}></div>
+            <div style={{backgroundColor: "#d60270"}}></div>
+            <div style={{backgroundColor: "#9b4f96"}}></div>
+            <div style={{backgroundColor: "#0038a8"}}></div>
+            <div style={{backgroundColor: "#0038a8"}}></div>
+        </div>,
+        <div className="stripes" direction="horizontal" style={{
+            width: '100%',
+            height: '100%',
+        }}>
+            <div style={{backgroundColor: "#5bcffb"}}></div>
+            <div style={{backgroundColor: "#f5abb9"}}></div>
+            <div style={{backgroundColor: "#ffffff"}}></div>
+            <div style={{backgroundColor: "#f5abb9"}}></div>
+            <div style={{backgroundColor: "#5bcffb"}}></div>
+        </div>,
+        <div className="stripes" direction="horizontal" style={{
+            width: '100%',
+            height: '100%',
+        }}>
+            <div style={{backgroundColor: "#000000"}}></div>
+            <div style={{backgroundColor: "#9ad9ea"}}></div>
+            <div style={{backgroundColor: "#00a3e8"}}></div>
+            <div style={{backgroundColor: "#b5e51d"}}></div>
+            <div style={{backgroundColor: "#ffffff"}}></div>
+            <div style={{backgroundColor: "#ffc90d"}}></div>
+            <div style={{backgroundColor: "#fc6667"}}></div>
+            <div style={{backgroundColor: "#feaec9"}}></div>
+            <div style={{backgroundColor: "#000000"}}></div>
+        </div>,
+        <AceFlag height='100%' />,
     ]
 
     return (
